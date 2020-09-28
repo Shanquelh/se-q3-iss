0c563b3ff91b76f221fc9233daea65ff8fadc5ea
SHANQUEL H. SCOTT <shanquelh@yahoo.com>
2020-09-28 17:14:04 -0500

    Completed part A and part B

--- iss.py
+++ iss.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'Shanquel Scott study hall John W.'
+
+import requests
+import turtle
+
+
+def astros():
+    workers = ""
+    r = requests.get('http://api.open-notify.org/astros.json').json()
+    print(f"There are a total of {r['number']} people in space")
+    for astros in r['people']:
+        workers += astros['name'] + ' is on ' + astros['craft'] + '\n'
+    print(workers)
+    # return workers
+
+
+def coords():
+    r = requests.get('http://api.open-notify.org/iss-now.json').json()
+    print(f"The current geographic {r['iss_position']['latitude']}")
+    print(f"The current geographic {r['iss_position']['longitude']}")
 
 
 def main():
-    pass
+    astros()
+    coords()
+    turtle.done()
 
 
 if __name__ == '__main__':
